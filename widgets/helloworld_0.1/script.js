2f02223b387b46f44dec99752a9e0eb28396cae3
Torin <torin@chumby.com>
2011-09-08 13:00:35 +0800

    netv-controlpanel: added HelloWord HTML widget template

---
+++ widgets/helloworld_0.1/script.js
@@ -0,0 +1,73 @@
+/*
+	Refer to NeTV API documentation for more information
+	[http://wiki.chumby.com/index.php/NeTV_developer_info]
+*/
+
+//------------------------------------------------------------------------
+// This function is called when widget is loaded
+//------------------------------------------------------------------------
+
+function onLoad()
+{
+	fDebug("Hello World HTML Widget loaded!");
+}
+
+
+//------------------------------------------------------------------------
+// Example: obtain parameters from Chumby server
+// Parameters from Chumby server (config widget) are passed as GET parameters	
+// This widget will be loaded as ./widgets/helloworld_0.1/index.html?param1=value1&param2=value2&param3=value3
+// To get 'username' variable being passed from Chumby server (config widget), simply use:
+//		var my_username = getParameterByName('username');
+//------------------------------------------------------------------------
+
+function doSomething()
+{
+	//Example: get 'secret' variable being passed from Chumby server (config widget)
+	//see utils.js
+	var my_secret = getGETParameterByName("secret");
+	if (my_secret != "")
+		fDebug("My secret: " + my_secret);
+	
+	//doSomethingElse();
+}
+
+
+//------------------------------------------------------------------------
+// Example: Use web services on http://localhost/bridge to interact with NeTV hardware
+// Refer to NeTV API documentation for web services available on http://localhost/bridge
+// [http://wiki.chumby.com/index.php/NeTV_web_services]
+// This example use jQuery library
+//------------------------------------------------------------------------
+
+function Hello()
+{
+	//see utils.js
+	xmlhttpPost("", "post", { 'cmd' : 'Hello' }, helloCallback );
+}
+
+function helloCallback(vData)
+{
+	if (vData.split("</status>")[0].split("<status>")[1] != "1")
+	{
+		fDebug("Error while receiving Hello command");
+		return;
+	}
+	
+	var helloData = new Array();
+	for (var idx in cNetConfig.instance.helloParamNamesArray)
+	{
+		var paramName = cNetConfig.instance.helloParamNamesArray[idx];
+		helloData[paramName] = vData.split("</"+paramName+">")[0].split("<"+paramName+">")[1];
+	}
+
+	//Print out the device info to console
+	fDebug("Firmware version: " + helloData['fwver']);
+	fDebug("Hardware version: " + helloData['hwver']);
+	fDebug("MAC Address: " + helloData['mac']);
+	fDebug("IP Address: " + helloData['ip']);
+	
+	//Show this info in the widget UI (jQuery)
+	var content_string = "Firmware version: " + helloData['fwver'] + " - " + "IP Address: " + helloData['ip'];
+	$("#div_info").html(content_string);
+}
---
+++ widgets/helloworld_0.1/utils.js
@@ -0,0 +1,114 @@
+
+// -------------------------------------------------------------------------------------------------
+//	Print out to console
+// -------------------------------------------------------------------------------------------------
+
+function fDebug(text_string)
+{
+	console.log("|~|" + text_string);
+}
+
+
+// -------------------------------------------------------------------------------------------------
+// Get a GET parameter by its name
+// -------------------------------------------------------------------------------------------------
+
+function getGETParameterByName(name)
+{
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.href);
+	if(results == null)
+		return "";
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//	Make a GET/POST request
+// -------------------------------------------------------------------------------------------------
+
+function xmlhttpPost(
+	vUrl,
+	vType,			// "post" | "get"
+	vData,
+	vCompleteFunc
+)
+{
+	var xmlHttpReq = getXHR();
+	if (xmlHttpReq == null) {
+		aleart("Failed to get XMLHttpRequest");
+		return;
+	}
+	
+	if (vUrl == '')
+		vUrl = "../bridge";
+	xmlHttpReq.open(vType, vUrl, true);
+	xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xmlHttpReq.onreadystatechange = function()
+	{
+		if (xmlHttpReq.readyState == 4)
+		{
+			switch (xmlHttpReq.status)
+			{
+				case 200:
+					if (vCompleteFunc)
+						vCompleteFunc(xmlHttpReq.responseText);
+					break;
+				case 0:
+					if (vCompleteFunc)
+						vCompleteFunc(0);
+					break;
+			}
+		}
+	}
+	
+	if (vType.toLowerCase() == "post")
+	{
+		var parameters = "";
+		for (var o in vData)
+			parameters += o + "=" + encodeURIComponent(vData[o]) + "&";	
+		parameters = parameters.substring(0, parameters.length-1);
+			
+		xmlHttpReq.send(parameters);
+	}
+	else
+	{
+		xmlHttpReq.send();
+	}
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//Get an XMLHttpRequest, object
+// -------------------------------------------------------------------------------------------------
+
+function getXHR()
+{
+	var req = null;
+	if(window.XMLHttpRequest)
+	{
+		req = new XMLHttpRequest();
+		return req;
+	}
+	else
+	{
+		try {
+			req = new ActiveXObject('Msxml2.XMLHTTP');		//IE
+			return req;
+		}
+		catch(e)
+		{
+			try {
+				req = new ActiveXObject("Microsoft.XMLHTTP");		//IE
+				return req;
+			}
+			catch(e)
+			{
+				return null;
+			}
+		}
+	}
+	return req;
+}
